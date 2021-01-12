2f5188889135cb06eb8acf89cf68102045843725
Ikoko <iko89@hotmail.de>
2021-01-12 12:49:40 +0100

    geolocation

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,81 +1,26 @@
-var pwaCard = document.querySelector('#pwa');
-var pwaCardContent = pwaCard.querySelector('.card__content');
-var pwaCardDetails = pwaCard.querySelector('.card__details');
-var detailsShown = false;
+// geolocation
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(function() {
-      console.log('SW registered');
-    });
+var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
 }
 
-pwaCard.addEventListener('click', function (event) {
-  if (!detailsShown) {
-    detailsShown = true;
-    pwaCardContent.style.opacity = 0;
-    pwaCardDetails.style.display = 'block';
-    pwaCardContent.style.display = 'none';
-    setTimeout(function () {
-      pwaCardDetails.style.opacity = 1;
-    }, 300);
-  } else {
-    detailsShown = false;
-    pwaCardDetails.style.opacity = 0;
-    pwaCardContent.style.display = 'block';
-    pwaCardDetails.style.display = 'none';
-    setTimeout(function () {
-      pwaCardContent.style.opacity = 1;
-    }, 300);
-  }
-});
-
-//camera and microphone
-
-function getUserMedia(constraints) {
-  // if Promise-based API is available, use it
-  if (navigator.mediaDevices) {
-    return navigator.mediaDevices.getUserMedia(constraints);
-  }
-    
-  // otherwise try falling back to old, possibly prefixed API...
-  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    
-  if (legacyApi) {
-    // ...and promisify it
-    return new Promise(function (resolve, reject) {
-      legacyApi.bind(navigator)(constraints, resolve, reject);
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
     });
-  }
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';
 }
+HTML
+<button id="askButton">Ask for location</button>
 
-function getStream (type) {
-  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-    alert('User Media API not supported.');
-    return;
-  }
-
-  var constraints = {};
-  constraints[type] = true;
-  
-  getUserMedia(constraints)
-    .then(function (stream) {
-      var mediaControl = document.querySelector(type);
-      
-      if ('srcObject' in mediaControl) {
-        mediaControl.srcObject = stream;
-      } else if (navigator.mozGetUserMedia) {
-        mediaControl.mozSrcObject = stream;
-      } else {
-        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-      }
-      
-      mediaControl.play();
-    })
-    .catch(function (err) {
-      alert('Error: ' + err);
-    });
-}
-
+<div id="target"></div>
